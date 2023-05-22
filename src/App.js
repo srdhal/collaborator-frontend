1d1638ecf91fae195a11b35ab6cdf6a94267f89d
unknown <soumyardhal03@gmail.com>
2023-05-22 17:01:38 +0530

    completed the basic layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import TextEditor from './components/TextEditor';
+import {BrowserRouter,Routes,Route, redirect, Navigate} from 'react-router-dom'
+import {v4 as uuidv4} from 'uuid'
 
 function App() {
-  return <TextEditor/>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/document/${uuidv4()}`} replace />}></Route>
+        <Route path="/document/:id" element={<TextEditor/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -2,6 +2,7 @@ import React, { useCallback, useState ,useEffect} from 'react'
 import 'quill/dist/quill.snow.css'
 import Quill from 'quill'
 import io from 'socket.io-client'
+import { useParams } from 'react-router-dom'
 
 const TOOLLBAR_OPTIONS= [
     [{header: [1,2,3,4,5,6,false]}],
@@ -21,32 +22,34 @@ export default function TextEditor() {
 
   const  [socket,setSocket]=useState()
   const  [quill,setQuill]=useState()
-
-
+  const {id: documentId}=useParams()
   const wrapperRef=useCallback((wrapper)=>{
-      if(wrapper==null) return
-      wrapper.innerHTML=""
-      const editor=document.createElement('div')
-      wrapper.append(editor)
-      const q=new Quill(editor,{theme:'snow',modules: {toolbar: TOOLLBAR_OPTIONS}})
-      setQuill(q)
-  },[])
-
+     if(wrapper==null) return
+     wrapper.innerHTML=""
+     const editor=document.createElement('div')
+     wrapper.append(editor)
+     const q=new Quill(editor,{theme:'snow',modules: {toolbar: TOOLLBAR_OPTIONS}})
+     q.disable()
+     q.setText("Loading...")
+     setQuill(q)
+     console.log(documentId)
+   },[])
+   
   useEffect(()=>{
      const s=io("http://localhost:5000")
      setSocket(s)
-
+     
      return()=>{
         s.disconnect()
-     }
-  },[])
-
-  useEffect(()=>{
-
-    if(socket==null || quill == null) return
-     const handleChange =(delta,oldDelta,source)=>{
-        if(source!=='user') return;
-
+      }
+   },[])
+   
+   useEffect(()=>{
+      
+      if(socket==null || quill == null) return
+      const handleChange =(delta,oldDelta,source)=>{
+         if(source!=='user') return;
+         
         socket.emit("send-text-changes",delta)
      }
 
@@ -70,6 +73,30 @@ export default function TextEditor() {
         socket.off("receive-text-changes",handleChange)
      }
   },[socket,quill])
+
+  useEffect(()=>{
+     if(socket==null || quill==null) return
+
+     socket.once("load-data",async document=>{
+        quill.setContents(document)
+        quill.enable()
+     })
+
+     socket.emit("get-data",documentId)
+  },[socket,quill,documentId])
+
+   useEffect(()=>{
+       if(socket==null || quill==null) return;
+
+       const timer=setInterval(()=>{
+          socket.emit("save-doc",quill.getContents())
+       },2000)
+
+       return ()=>{
+         clearInterval(timer)
+       }
+
+   },[socket,quill])
 
 
   return (
